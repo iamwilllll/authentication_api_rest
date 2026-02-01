413b0e00b823e9249757a967ab4d424362312221
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-01-31 20:12:21 -0400

    Update SMTP configuration in env and sendEmail service

--- src/config/env.ts
+++ src/config/env.ts
@@ -19,4 +19,12 @@ export const env = {
         THREE_HOURS: 1000 * 60 * 60 * 3,
         THREE_DAYS: 1000 * 60 * 60 * 24 * 3,
     },
+
+    SMTP: {
+        HOST: process.env.SMTP_HOST || '',
+        USER: process.env.SMTP_USER || '',
+        PASSWORD: process.env.SMTP_PASSWORD || '',
+        PORT: process.env.NODE_ENV === 'development' ? 587 : 465,
+        SECURE: process.env.NODE_ENV === 'development' ? false : true,
+    },
 };
--- src/services/sendEmail.service.ts
+++ src/services/sendEmail.service.ts
@@ -1,32 +1,29 @@
 import nodemailer from 'nodemailer';
+import { env } from '../config/env.js';
 
 type SendEmailServiceProps = {
-    from: string;
     to: string;
     subject: string;
     text: string;
     html: string;
 };
 
+const { HOST, PORT, SECURE, USER, PASSWORD } = env.SMTP;
+
 const configOptions = {
-    host: 'smtp.example.com',
-    port: 587,
-    secure: false,
+    host: HOST,
+    port: PORT,
+    secure: SECURE,
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        user: USER,
+        pass: PASSWORD,
     },
 };
-export async function sendEmailService({ from, to, subject, text, html }: SendEmailServiceProps) {
-    try {
-        const transporter = nodemailer.createTransport(configOptions);
-        const isConnected = await transporter.verify();
-        if (isConnected) throw new Error('Transporter not ready');
 
-        const info = await transporter.sendMail({ from, to, subject, text, html });
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    } catch (err) {
-        console.error('Error while sending mail', err);
-    }
+export async function sendEmailService({ to, subject, text, html }: SendEmailServiceProps) {
+    const transporter = nodemailer.createTransport(configOptions);
+    await transporter.verify();
+
+    const info = await transporter.sendMail({ from: 'E-commerce', to, subject, text, html });
+    return info;
 }
